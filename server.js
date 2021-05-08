574b0f12b63a25d6013665a40431e27136af8bf7
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-05-08 11:42:00 -0700

    prepare server to run handlebarchanges

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const env = require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+//const routes = require("./routes");
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+//app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
