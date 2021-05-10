b906bfa911ae83e2b85d78bb4bcbf68dbbad5254
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-05-10 13:45:38 -0700

    updating server

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const exphbs = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const env = require("dotenv").config();
-const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 //const routes = require("./routes");
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,5 +21,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
