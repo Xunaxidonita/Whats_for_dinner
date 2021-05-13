89d78cf93c41849c15aa9fa00191a81da6cd168a
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2021-05-13 11:23:45 -0700

    restructured file

--- server.js
+++ server.js
@@ -8,19 +8,18 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: process.env.SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: false,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
