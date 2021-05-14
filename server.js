16357deebfcce3ecbe9f1124a00f60ed20981251
jessicap5159 <jessicaj5159@gmail.com>
2021-05-14 15:17:23 -0700

    troubleshooting CSS problem

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.set('view engine', 'handlebars');
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
